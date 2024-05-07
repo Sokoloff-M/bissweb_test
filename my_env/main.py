49866419ff0ba931da74da12ba57cfd23dd7a6c0
Sokoloff-M <kutuzoff38@mail.ru>
2024-05-07 20:09:52 +0800

    first commit

---
+++ my_env/main.py
@@ -0,0 +1,14 @@
+
+
+#Отображает баланс
+def balance():
+    pass
+#Добавение записи
+def Adding_an_entry():
+    pass
+#Редактирование записи
+def Editing_an_entry():
+    pass
+#Поиск по записи
+def Search_through_records():
+        pass
